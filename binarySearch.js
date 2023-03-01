ca561e505d38c65ac92972bc8e286ca7d6636365
Сергей Вилков <sergey.vilkov@rsv.ru>
2023-03-01 21:16:22 +0300

    Binary

---
+++ binarySearch.js
@@ -0,0 +1,21 @@
+const numbersList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+function binarySearch({ arr, target }) {
+    let left = 0;
+    let right = arr.length;
+    let mid;
+
+    while( left <= right) {
+        mid = Math.round((right - left) / 2) + left; // ((10 - 0 )/ 2 )+ 0
+
+        if(target === arr[mid]) {
+            return mid;
+        } else if (target < arr[mid]) {
+            right = mid - 1;
+        } else {
+            left = mid - 1;
+        }
+    }
+}
+
+console.log(binarySearch({ arr: numbersList, target: 4}));
